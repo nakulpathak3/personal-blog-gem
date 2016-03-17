055f65c2fa332427418a6136a813a4346d1e0b0b
Nakul Pathak <nakulpathak3@hotmail.com>
2016-03-16 22:56:53 -0400

    Fix comment destruction and bump version to 0.0.3

--- app/controllers/personal_blog/comments_controller.rb
+++ app/controllers/personal_blog/comments_controller.rb
@@ -14,6 +14,13 @@ module PersonalBlog
       end
     end
 
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@post), notice: "Comment was successfully destroyed."
+    end
+
     private
     def comment_params
       params.require(:comment).permit(:text, :author)
